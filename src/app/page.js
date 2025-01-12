a86b32eab4d3aee1ebf47e3b03a1d539070dbf05
kikerkov <galacticomnivore@gmail.com>
2025-01-12 22:17:10 +0100

    Trigger Build

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
+
 import background1Img from "@/assets/background1.png";
 import heroImg from "@/assets/hero1.png";
 import avatar1Img from "@/assets/avatar1.png";
