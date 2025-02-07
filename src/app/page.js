f7ce9996a9fb91edc82f2fe435fa25b9a8baa846
kikerkov <galacticomnivore@gmail.com>
2025-02-07 16:49:29 +0100

    fadsf

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState, useRef } from "react";
 
+
 import background1Img from "@/assets/background1.png";
 import heroImg from "@/assets/HERO.png";
 import avatar1Img from "@/assets/avatar1.png";
