bd05d5804037108f037278832f0429c6ffcf9f58
Dejan Gavrilovic <dejan.gavrilovikk@gmail.com>
2025-02-07 16:36:55 +0100

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "../globals.css";
 import { Inter, Orbitron } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 const orbitron = Orbitron({ subsets: ["latin"] });
@@ -8,7 +9,10 @@ const orbitron = Orbitron({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <title>Galactic Omnivore</title>
+      </Head>
       <body className={orbitron.className}>
         <ThemeProvider attribute="class" defaultTheme="system">
           {children}
