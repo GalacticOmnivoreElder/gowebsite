781f7af2ca7e734fd89978edb185d2c9989d023e
Galactic Omnivore <194530138+GalacticOmnivoreElder@users.noreply.github.com>
2025-06-30 19:01:28 +0200

    Git Ready to Test

--- src/app/(main)/profile/page.js
+++ src/app/(main)/profile/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { useState, useEffect, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
