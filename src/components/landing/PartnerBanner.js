2ca9142728111df7a4142b3616e191b111735b1b
Galactic Omnivore <194530138+GalacticOmnivoreElder@users.noreply.github.com>
2025-07-06 22:35:23 +0200

    Update Partner Banners

--- src/components/landing/PartnerBanner.js
+++ src/components/landing/PartnerBanner.js
@@ -6,8 +6,8 @@ import Image from "next/image";
 const partnerLogos = [
   { src: "/logos/logo1.png", alt: "Partner 1" },
   { src: "/logos/logo2.png", alt: "Partner 2" },
-  { src: "/logos/logo3.png", alt: "Partner 3" },
-  { src: "/logos/logo4.png", alt: "Partner 4" },
+  //{ src: "/logos/logo3.png", alt: "Partner 3" },
+  //{ src: "/logos/logo4.png", alt: "Partner 4" },
   { src: "/logos/logo5.png", alt: "Partner 5" },
   { src: "/logos/logo6.png", alt: "Partner 6" },
   { src: "/logos/logo7.png", alt: "Partner 7" },
